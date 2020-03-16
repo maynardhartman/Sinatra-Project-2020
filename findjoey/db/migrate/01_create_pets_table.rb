4c996873e5cc1c4e75820133d07f8ea00bb7ace9
maynardhartman <mjhartman@brighthouse.com>
2020-03-16 15:22:04 -0400

    configured environment.rb

--- findjoey/db/migrate/01_create_pets_table.rb
+++ findjoey/db/migrate/01_create_pets_table.rb
@@ -1,74 +1,46 @@
 # frozen_string_literal: true
 
+
+
+
+
+
+
 class CreatePets < ActiveRecord::Migration[5.2]
 
 
 
-  def Pets
-
-
+  def change
 
     create_table pets do |p| (
 
-
-
       p.integer   :id           #system generated
-
-
 
       p.string    :pet_name
 
-
-
       p.string    :pet_breed 
-
-
 
       p.integer   :pet_weight
 
-
-
       p.string    :pet_temperament
-
-
 
       p.boolean   :pet_chipped,  default: true
 
-
-
       p.boolean   :pet_collared, default: true
-
-
 
       p.string    :chip_id
 
-
-
       p.string    :pet_color
-
-
 
       p.datetime  :pet_went_missing, default: datetime.now()  # the date and time pet went missing default: current datatime
 
-
-
       p.datetime  :pet_date_found, defaullt: datetime.new()   # the date and time pet was found if known defalut: current datetime
-
-
 
       p.timestamps 
 
-
-
     ) 
-
-
 
   end
 
-
-
 end
-
-
 
