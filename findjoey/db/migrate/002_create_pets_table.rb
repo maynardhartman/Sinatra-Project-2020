ce61eaa6c30500c9c000329903619547a3d45b74
maynardhartman <mjhartman@brighthouse.com>
2020-03-18 13:40:24 -0400

    no message

--- findjoey/db/migrate/002_create_pets_table.rb
+++ findjoey/db/migrate/002_create_pets_table.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class CreatePets < ActiveRecord::Migration[5.2]
+class CreatePetsTable < ActiveRecord::Migration[5.2]
   def change
     create_table :pets do |t|
       t.string :name
