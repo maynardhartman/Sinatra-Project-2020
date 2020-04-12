e490d54e4bae427630e7474e4e7e9f33aa946b8f
maynardhartman <mjhartman@brighthouse.com>
2020-04-12 07:03:43 -0400

    made some changes to main.css and basic config

--- findjoey/app/models/pet.rb
+++ findjoey/app/models/pet.rb
@@ -1,5 +1,5 @@
 class Pet < ActiveRecord::Base
-  
-  validates :pet_chipped, presence: true # uniqueness: { case_insensitive: true }
+  validates :pet_chipped, presence: true
   validates :pet_chip_id, presence: true, uniqueness: { numericality: true }
+  has_one  :owner
 end
--- findjoey/app/models/user.rb
+++ findjoey/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :name presence: true, uniqueness: { case_sensitive: false }, length: {maximum: 30}
+  validates :breed presence: true
+  
+  
+  
+  has_many :pets
 end
